33e0d28393a5b531ad01e90e8981c85e73963545
rnb2022 <mdb1710@gmail.com>
2023-08-03 12:07:39 -0400

    hero section

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+const HeroSection = () => {
+    return(
+        <>
+          <div className='flex min-w-screen flex-col place-items-center bg-orange-300  h-96  my-20 py-10'>
+            <h1 className="text-7xl font-bold">Welcome to Beat$Tacks</h1>
+            <p className="text-xl font-bold md:text-3xl">Helping Indie Music Artist BTC your fanbase</p>
+          </div>
+        </>
+    )
+}
+
+export default HeroSection
